ae9d2a2bb7561e6f6e2e089f4fa7b1a823729fc6
shyzus <dev@shyzus.com>
2020-01-03 03:33:49 +0100

    initial commit

---
+++ app/src/main/java/nl/hva/fdmci/mobiledevelopment/level_5_example/Reminder.kt
@@ -0,0 +1,18 @@
+package nl.hva.fdmci.mobiledevelopment.level_5_example
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "reminderTable")
+data class Reminder(
+    @ColumnInfo(name = "reminder")
+    var reminder: String,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+
+) : Parcelable
